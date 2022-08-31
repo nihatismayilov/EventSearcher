a344deffe41645bd852c406f85e9edfb7fd93068
Nazim Asadov <nazimatmu@gmail.com>
2022-08-31 21:22:24 +0400

    Sonar added third

--- EventSearcher/AppDelegate.swift
+++ EventSearcher/AppDelegate.swift
@@ -14,12 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         // Nzm change to router after injection
         window?.rootViewController = FirstViewController()
         return true
+
+
+        
     }
 }
--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source "https://rubygems.org"
 gem "fastlane"
 gem "slather"
 
-plugins_path = File.join(File.dirname(_FILE_), 'fastlane', 'Pluginfile')
+plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
 eval_gemfile(plugins_path) if File.exist?(plugins_path)
